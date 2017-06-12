523491cf8ed12a077283150f878bc417c4c29975
rekaru <rekaru@protonmail.ch>
2017-06-12 13:22:54 +0300

    Update posts reducer

--- src/reducers/posts.js
+++ src/reducers/posts.js
@@ -1,5 +1,18 @@
-const posts = (state=[], action) => {
+const posts = (state = [], action) => {
+  const i = action.index
 
+  switch (action.type) {
+    case 'INCREMENT_LIKES':
+      return [
+        ...state.slice(0, i),
+        { ...state[i],
+          likes: state[i].likes + 1
+        },
+        ...state.slice(i + 1),
+      ]
+    default:
+      return state
+  }
 }
 
 export default posts
